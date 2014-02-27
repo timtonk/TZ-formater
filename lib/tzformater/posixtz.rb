69943cbcc3e5ceceb1f610246e006963555c0810
Dmitry Tonkonogov <omun.spambox@gmail.com>
2014-02-27 20:50:34 +1100

    Set rspec as default rake task, add constructor to posixtz class

--- lib/tzformater/posixtz.rb
+++ lib/tzformater/posixtz.rb
@@ -1,5 +1,9 @@
 module TZFormater
   class PosixTZ
     @@storage = TZFormater::PstoreAdapter.new(TZFormater::FILENAME)
+
+    def initialize(posix_timezone)
+      @tz = posix_timezone
+    end
   end
 end
