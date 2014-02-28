04a69cdf31774a0e230bdba6e562b87d0fa3ee5c
Dmitry Tonkonogov <omun.spambox@gmail.com>
2014-02-28 14:18:37 +1100

    Little refactoring for existed tests
    Added some new cases, remove useless config section in spec_helper

--- spec/case/posix_spec.rb
+++ spec/case/posix_spec.rb
@@ -1,19 +1,45 @@
 require 'spec_helper'
 
 describe TZFormater::PosixTZ do
-  let(:tz) { 'VLAT-11' }
+  let(:tz)          { 'MST7' } # Arizona
+  let(:posix_class) { TZFormater::PosixTZ }
+  let(:posix_tz)    { posix_class.new(tz) }
 
-  context 'PosixTZ.all' do
-    subject { TZFormater::PosixTZ }
+  context 'class method' do
+    subject { posix_class }
 
-    it 'contains one of timezone' do
-      expect(subject.all).to include(tz)
+    context '.all' do
+      let(:tz) { 'VLAT-11' }
+
+      it 'contains one of timezone' do
+        expect(subject.all).to include(tz)
+      end
+    end
+
+    context '.new' do
+      context 'with incorrect tz' do
+        let(:tz) { 'random stuff' }
+
+        it 'raises an ArgumentError' do
+          expect { posix_tz }.to raise_error(ArgumentError)
+        end
+
+        it 'raises with correct message' do
+          expect { posix_tz }.to raise_error('Unknown POSIX timezone')
+        end
+      end
+
+      context 'with correct tz' do
+        it 'creates valid object' do
+          expect(posix_tz).to be_an_instance_of(posix_class)
+        end
+      end
     end
   end
 
-
   context 'with one-to-one mapped timezone' do
-    subject { TZFormater::PosixTZ.new(tz) }
+    let(:tz) { 'VLAT-11' }
+    subject { posix_tz }
 
     it 'returns olson-like string' do
       expect(subject.to_olson).to include('Asia/Vladivostok')
@@ -40,4 +66,32 @@ describe TZFormater::PosixTZ do
     end
   end
 
+  context 'with many-to-many mapped timezone' do
+    let(:tz) { 'CET-1CEST,M3.5.0,M10.5.0/3' }
+    subject { posix_tz }
+
+    it 'returns olson-like string' do
+      expect(subject.to_olson).to include('Europe/Berlin')
+    end
+
+    it 'returns many olson-like string' do
+      expect(subject.to_olson.size).to eql(4)
+    end
+
+    it 'returns win-like string' do
+      expect(subject.to_win).to include('(UTC+01:00) Belgrade, Bratislava, Budapest, Ljubljana, Prague')
+    end
+
+    it 'returns many win-like string' do
+      expect(subject.to_win.size).to eql(4)
+    end
+
+    it 'returns win registry string' do
+      expect(subject.to_win_reg).to include('Romance Standard Time')
+    end
+
+    it 'returns many win registry string' do
+      expect(subject.to_win_reg.size).to eql(4)
+    end
+  end
 end
