f6d95023fd4b9ad3522f41fd4b1ebbb720cb3a97
Dmitry Tonkonogov <omun.spambox@gmail.com>
2014-03-03 18:30:42 +1100

    create timezone parent class
    update posixtz

--- lib/tzformater/posixtz.rb
+++ lib/tzformater/posixtz.rb
@@ -1,26 +1,5 @@
 module TZFormater
-  class PosixTZ
-    @@storage = TZFormater::PstoreAdapter.new(TZFormater::FILENAME)
-
-    def self.all
-      @@storage.get_all(:posix)
-    end
-
-    def initialize(tz = 'Etc/GMT')
-      @info = @@storage.get_by(:posix, tz)
-      raise ArgumentError.new('Unknown POSIX timezone') if @info.nil?
-    end
-
-    def to_olson
-      @info[:olson]
-    end
-
-    def to_win
-      @info[:win]
-    end
-
-    def to_win_reg
-      @info[:win_reg]
-    end
+  class PosixTZ < TZFormater::Timezone
+    @@key = :posix
   end
 end
