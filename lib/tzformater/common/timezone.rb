3d617215cf985c9a018c8b1535e810aa8396b90b
Dmitry Tonkonogov <omun.spambox@gmail.com>
2014-03-05 09:57:48 +1100

    Update timezone class
    Make instantiate in .all method more optimal
    Update named methods to return an instance of the corresponding class
    instead of string

--- lib/tzformater/common/timezone.rb
+++ lib/tzformater/common/timezone.rb
@@ -6,19 +6,41 @@ module TZFormater
       @@storage = TZFormater::Common::PstoreAdapter.new(TZFormater::FILENAME)
 
       def self.all
-        @@storage.get_all(@key).map{|tz| self.new(tz) }
+        @@storage.get_all(@key).map{|key, value| self.new(key, value) }
       end
 
-      def initialize(tz)
+      def initialize(tz, info = nil)
         @name = tz
-        @info = @@storage.get_by(@key, @name)
+        @info = info || @@storage.get_by(@key, @name)
         raise ArgumentError.new("Unknown #{@key} timezone") if @info.nil?
       end
 
-      [:posix, :olson, :win, :win_reg].each do |type|
-        define_method(type) do
-          type == @key ? self : @info[type]
+      def posix
+        return self if @key == :posix
+        TZFormater::PosixTZ.new(@info[:posix])
+      end
+
+      def olson
+        return self if @key == :olson
+        if @info[:olson].kind_of? Array
+          @info[:olson].map{|tz| TZFormater::OlsonTZ.new(tz) }
+        else
+          TZFormater::OlsonTZ.new(@info[:olson])
         end
+      end
+
+      def win
+        return self if @key == :win
+        if @info[:win].kind_of? Array
+          @info[:win].map{|tz| TZFormater::WinTZ.new(tz)}
+        else
+          TZFormater::WinTZ.new(@info[:win])
+        end
+      end
+
+      def win_reg
+        return self if @key == :win_reg
+        @info[:win_reg].map{|tz| TZFormater::WinRegTZ.new(tz)}
       end
 
       def offset
